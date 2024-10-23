b3886f1c4deb439664288bc9823f98584e15328e
Antony-Silvesta <antonysilvesta74@gmail.com>
2024-10-23 20:13:40 +0530

    Fully updated all

--- Google.py
+++ Google.py
@@ -3,17 +3,15 @@ from selenium import webdriver
 def test_google_search():
     # Set Chrome options
     options = webdriver.ChromeOptions()
-    # You can add any other options if needed, but for using only Chrome, no need for headless or remote
 
     # Initialize the Chrome WebDriver
     driver = webdriver.Chrome(options=options)  # This will launch a Chrome browser window
 
     # Open Google and verify the title
     driver.get("https://www.google.com")
-    assert "Google" in driver.title
+    assert "Google" in driver.title, "Google search page did not load properly"
     
     # Close the browser
     driver.quit()
 
-# Run the test
-test_google_search()
+# No need to manually call test_google_search(), pytest will discover and run this test
